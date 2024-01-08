a17e0409abc65ecd0457397fbfe6292a38ff0216
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-01-08 02:33:24 +0200

    ✍️Added navbar

--- static/js/dev/app.js
+++ static/js/dev/app.js
@@ -2,11 +2,40 @@ import Calendar from "./calendar.js";
 
 class App {
     constructor() {
+        document.addEventListener('DOMContentLoaded', (event) => this.main(event));
         this.calendar = new Calendar("#calendar");
     }
 
     run() {
         this.calendar.render();
+    }
+
+    async main() {
+
+        
+        // Burger Menu Start
+        const burgerButton = document.querySelector(".header__burger-menu");
+        const header__nav = document.querySelector(".header__nav");
+        const body = document.querySelector("body");
+        const navLinks = document.querySelectorAll(".header__menu-link");
+
+        burgerButton.addEventListener("click", function() {
+            header__nav.classList.toggle("active");
+            burgerButton.classList.toggle("active");
+            body.classList.toggle("lock");
+        });
+
+        navLinks.forEach((navLink) => {
+            navLink.addEventListener("click", () => {
+                if (burgerButton.classList.contains("active")) {
+                    header__nav.classList.remove("active");
+                    burgerButton.classList.remove("active");
+                    body.classList.remove("lock");
+                }
+            });
+        });
+        // Burger Menu End
+
     }
 }
 
